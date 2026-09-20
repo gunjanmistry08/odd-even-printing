6f0a672e6bca2c145ab34c141a1a61fb6f1fa66f
gunjanmistry08 <gunjanmistry008@gmail.com>
2026-09-20 21:38:20 +0530

    golang change method to channels

--- golang/main.go
+++ golang/main.go
@@ -1,25 +1,31 @@
 package main
 
-import (
-	"sync"
-)
+func print_routine(start_number int, firstTurn, secondTurn, done chan struct{}) {
+	for i := start_number; i <= 100; i = i + 2 {
+		<-firstTurn
 
-func print_routine(start_number int, first_lock, second_lock *sync.Mutex, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := start_number; i < 100; i = i + 2 {
-		second_lock.Lock()
 		println(i)
-		first_lock.Unlock()
+		if i != 100 {
+			secondTurn <- struct{}{}
+		}
+
 	}
+	done <- struct{}{}
 }
 
 func main() {
-	var odd_lock = sync.Mutex{}
-	var even_lock = sync.Mutex{}
-	var wg sync.WaitGroup
-	odd_lock.Lock()
-	wg.Add(2)
-	go print_routine(0, &odd_lock, &even_lock, &wg)
-	go print_routine(1, &even_lock, &odd_lock, &wg)
-	wg.Wait()
+
+	even_turn := make(chan struct{})
+	odd_turn := make(chan struct{})
+
+	done := make(chan struct{})
+
+	go print_routine(0, even_turn, odd_turn, done)
+	go print_routine(1, odd_turn, even_turn, done)
+
+	even_turn <- struct{}{}
+
+	<-done
+	<-done
+
 }
